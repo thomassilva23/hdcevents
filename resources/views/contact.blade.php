fa414e1aaf5d84a51c2790eaf0071361ab0969cd
Thomas Silva <thomassilva23@hotmail.com>
2026-02-03 10:41:40 -0300

    feat: - Adicionado Layout inicial e páginas de Contato e Produtos; - Adicionado logo SVG e imagens na pasta public; - Criado um arquivo Javascript vazio para futuros scripts;
    chore:
    - Adicionado integração do Bootstrap;

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+
+@section('title', 'Contato')
+
+@section('content')
+    <h1>Página de contato</h1>
+    <a href="/">Voltar para a home</a>
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('title')</title>
+
+    {{-- Fonte do Google --}}
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto">
+
+    {{-- CSS Bootstrap --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+    {{-- CSS da aplicação --}}
+    <link rel="stylesheet" href="/css/styles.css">
+    <script src="/js/scripts.js"></script>
+</head>
+
+<body>
+    @yield('content')
+    <footer>
+        <p>HDC Events &copy; 2026</p>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
