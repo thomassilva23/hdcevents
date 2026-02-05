fdd82b7572e0904138a3e017e4542ed664f1c131
Thomas Silva <thomassilva23@hotmail.com>
2026-02-05 17:06:34 -0300

    feat: Exibição de eventos com funcionalidade de pesquisa; Layout da Página Inicial aprimorado; Inserção de estilos;

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,31 @@
 
 @section('content')
 
-    @foreach ($events as $event)
-        <p>{{ $event->title }} -- {{ $event->description }}</p>
-    @endforeach
+    <div id="search-container" class="col-md-12">
+        <h1>Busque um evento</h1>
+
+        <form action="">
+            <input type="text" name="search" id="search" class="form-control" placeholder="Procurar...">
+        </form>
+    </div>
+
+    <div id="events-container" class="col-md-12">
+        <h2>Próximos eventos</h2>
+        <p class="subtitle">Veja os eventos dos próximos dias</p>
+
+        <div id="cards-container" class="row">
+            @foreach ($events as $event)
+                <div class="card col-md-3">
+                    <img src="/img/event-placeholder.jpg" alt="{{ $event->title }}">
+                    <div class="card-body">
+                        <p class="card-date">{{ $event->date }}</p>
+                        <h5 class="card-title">{{ $event->title }}</h5>
+                        <p class="card-participants">{{ $event->participants }} Participantes</p>
+                        <a href="/events/{{ $event->id }}" class="btn btn-primary">Saber mais</a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 
 @endsection
