0de8ba61d97ac7fd73fb0e92bd27a7f731687ce8
Thomas Silva <thomassilva23@hotmail.com>
2026-02-10 11:30:06 -0300

    feat: - Implementação de funcionalidade de criação e exibição de eventos; - Implementada funcionalidade de envio de imagens para eventos; - Estilos visuais para página de eventos;

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -3,5 +3,35 @@
 @section('title', 'Criar evento')
 
 @section('content')
-    <h1>Crie um evento</h1>
+    <div id="event-create-container" class="col-md-6 offset-md-3">
+        <h1>Crie o seu evento</h1>
+        <form action="/events" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group">
+                <label for="image">Imagem do Evento:</label>
+                <input type="file" id="image" class="form-control-file" name="image">
+            </div>
+            <div class="form-group">
+                <label for="title">Evento:</label>
+                <input type="text" id="title" class="form-control" name="title" placeholder="Nome do evento">
+            </div>
+            <div class="form-group">
+                <label for="city">Cidade:</label>
+                <input type="text" id="city" class="form-control" name="city" placeholder="Local do evento">
+            </div>
+            <div class="form-group">
+                <label for="private">O evento é privado?</label>
+                <select name="private" id="private" class="form-control">
+                    <option value="0">Não</option>
+                    <option value="1">Sim</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="description">Descrição:</label>
+                <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento?"></textarea>
+            </div>
+            
+            <input type="submit" value="Criar Evento" class="btn btn-primary">
+        </form>
+    </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
         <div id="cards-container" class="row">
             @foreach ($events as $event)
                 <div class="card col-md-3">
-                    <img src="/img/event-placeholder.jpg" alt="{{ $event->title }}">
+                    <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
                     <div class="card-body">
                         <p class="card-date">{{ $event->date }}</p>
                         <h5 class="card-title">{{ $event->title }}</h5>
