46b8935d6c6a24d5cc9bd840022826e9cfe6798e
Thomas Silva <thomassilva23@hotmail.com>
2026-02-19 17:02:15 -0300

    feat: - Criação de rota de deletar evento;
    fix:
     - Visual dos botões de deletar e editar evento.

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -80,4 +80,11 @@ class EventController extends Controller
 
         return view('events.dashboard', ['events' => $events]);
     }
+
+    public function destroy($id)
+    {
+        Event::findOrFail($id)->delete();
+
+        return redirect('/dashboard')->with('msg', 'Evento deletado com sucesso!');
+    }
 }
